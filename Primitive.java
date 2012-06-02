013c1ce311bb4ab3a301114b2eb10465a16d5e2a
Yuichiro Yokomaku <reyt_ms@hotmail.com>
2012-06-02 22:40:24 +0900

    test commited

---
+++ Primitive.java
@@ -0,0 +1,17 @@
+public class Primitive{
+   public static void main(String args[]){
+   		int i = '1';
+		char c = 49;
+        
+		System.out.println("i:"+i+" c:"+c);
+
+		/*
+			さて実行結果は？
+			1: 3行目でコンパイルエラー
+			2: 4行目でコンパイルエラー
+			3: 正常に実行される
+			4: コンパイルはされるが実行できない
+		*/
+	}
+}
+
